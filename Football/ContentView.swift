bbfe4430dc43e15030e41f92cd5c450e79ef2fa1
Jimmy Lin <jimmy72527@icloud.com>
2025-09-28 16:12:13 -0700

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Jimmy Lin on 9/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jimmy Lin on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
